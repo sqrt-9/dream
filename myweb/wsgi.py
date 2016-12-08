534247a08e99b82985ea2d5bfb5e2a6ff1bc4b49
root <root@localhost.localdomain>
2016-12-08 15:59:35 +0800

    server

--- myweb/wsgi.py
+++ myweb/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+from os.path import join,dirname,abspath
 from django.core.wsgi import get_wsgi_application
+import sys
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myweb.settings")
 
